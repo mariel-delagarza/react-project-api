99c20352dd144788cb2e9f47203989c9e82a8728
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 17:37:08 -0400

    Run migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_210326) do
+ActiveRecord::Schema.define(version: 2019_10_12_213211) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "question"
+    t.string "topic"
+    t.string "source_link"
+    t.text "answer"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
