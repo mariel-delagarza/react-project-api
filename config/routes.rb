00942dc02bd8a2b43e26f750ba1903ca6484e4d6
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 17:09:55 -0400

    Add explanation comment re: namespacing and first attempt at namespacing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  # I want my api to be questions-answered.com/api/v1/resource
+  # We want it to be clear that this is an api, and to not
+  # expect HTML to be rendered here, and having a version
+  # number is helpful because APIs are updated frequently.
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
