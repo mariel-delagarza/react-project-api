3615b551ddfe1e7f9a298b25d245dc3e2cb226c7
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 20:05:10 -0400

    Created mock user log_in - will replace later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+   
+  helpers do
+    #mock version of being logged in
+    def current_user
+      User.first 
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
