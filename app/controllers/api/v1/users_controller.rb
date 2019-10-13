c3e9bc1ad30b02cf657038834f30de71673388e3
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 18:17:22 -0400

    Correct password_digest in params to be password

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -48,6 +48,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :username, :password_digest)
+      params.require(:user).permit(:name, :username, :password)
     end
 end
