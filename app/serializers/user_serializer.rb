97866cd1aac7bad87cb2677de87858b0e77691bd
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 17:46:24 -0400

    Add presence validations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,6 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username
   has_many :answers, serializer: AnswerSerializer
+
+  validates :name, :username, presence: true
 end
