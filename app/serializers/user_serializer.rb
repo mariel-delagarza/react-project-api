73158a5f364a530d8472ec6ca197d3d2549e8410
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 17:48:16 -0400

    Remove validations from serializer to move into model

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,4 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username
   has_many :answers, serializer: AnswerSerializer
-
-  validates :name, :username, presence: true
 end
