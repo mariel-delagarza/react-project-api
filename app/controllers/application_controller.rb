cd73ae12480e3a5822fa4dc028f4c058a72b2558
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 23:16:51 -0400

    Remove unneeded comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::API
    
-#  helpers do
-    #mock version of being logged in
-    def current_user
-      User.find(session[:user_id])
-    end
+  def current_user
+    User.find(session[:user_id])
+  end
 
-    def logged_in?
-      !!current_user
-    end
- # end
+  def logged_in?
+    !!current_user
+  end
+
 end
